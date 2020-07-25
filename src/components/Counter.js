c354d2af94fe15855e09169b8847da51421a9869
Jackson Hardaker <jacksonhardaker@Jacksons-13-inch-MacBook-Pro.local>
2020-07-24 22:07:08 -0400

    force optimize.activate on ever counter click

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
+import { useGoogleOptimize } from "../context/WithGoogleOptimize";
 
 export const Counter = ({ parentCount, parentSetCount }) => {
   const [count, setCount] = useState(0);
+  const { activate } = useGoogleOptimize();
+
+  const onClick = () => {
+    parentSetCount ? parentSetCount(count => count + 1) : setCount(count => count + 1)
+    activate();
+  };
   return (
     <>
       <h2>{parentCount || count}</h2>
-      <button onClick={() => parentSetCount ? parentSetCount(count => count + 1) : setCount(count => count + 1)}>Increase Count</button>
+      <button onClick={onClick}>Increase Count</button>
     </>
   )
 };
