e7bf72918f8e9145813e3f9af76176b38162b969
Jackson Hardaker <jacksonhardaker@Jacksons-13-inch-MacBook-Pro.local>
2020-07-24 21:16:14 -0400

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 const _App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-164498009-2');
+  }, []);
   return (
     <>
       <Head>
         <script src="https://www.googleoptimize.com/optimize.js?id=OPT-KFBTF6S"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164498009-2"></script>
       </Head>
       <Component {...pageProps} />
     </>
