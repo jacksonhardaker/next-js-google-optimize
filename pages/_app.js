9945a184cd15ef27b4b5b1e416850d81a61aea63
Jackson Hardaker <jacksonhardaker@Jacksons-13-inch-MacBook-Pro.local>
2020-07-24 21:54:21 -0400

    extract optimize logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,11 @@
-import Head from 'next/head';
-import { useEffect } from 'react';
+import { WithGoogleOptimize } from '../src/context/WithGoogleOptimize';
 
 const _App = ({ Component, pageProps }) => {
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() { dataLayer.push(arguments); }
-    gtag('js', new Date());
-    gtag('config', 'UA-164498009-2');
-  }, []);
-
-  useEffect(() => {
-    window?.dataLayer?.push({'event': 'optimize.activate'});
-    console.log('App re-rendered');
-  });
   return (
     <>
-      <Head>
-        <script src="https://www.googleoptimize.com/optimize.js?id=OPT-KFBTF6S"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164498009-2"></script>
-      </Head>
-      <Component {...pageProps} />
+      <WithGoogleOptimize optimizeKey="OPT-KFBTF6S" analyticsKey="UA-164498009-2">
+        <Component {...pageProps} />
+      </WithGoogleOptimize>
     </>
   )
 };
---
+++ src/context/WithGoogleOptimize.js
@@ -0,0 +1,43 @@
+import Head from 'next/head';
+import { createContext, useContext, useEffect } from 'react';
+
+export const GoogleOptimizeContext = createContext();
+
+export const WithGoogleOptimize = ({ children, optimizeKey, analyticsKey }) => {
+
+  const value = {
+    optimizeKey,
+    analyticsKey,
+  };
+
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', analyticsKey);
+  }, []);
+
+  useEffect(() => {
+    window?.dataLayer?.push({'event': 'optimize.activate'});
+  });
+
+  return (
+    <GoogleOptimizeContext.Provider value={value}>
+      <Head>
+        <script src={`https://www.googleoptimize.com/optimize.js?id=${optimizeKey}`}></script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${analyticsKey}`}></script>
+      </Head>
+      {children}
+    </GoogleOptimizeContext.Provider>
+  );
+};
+
+export const useGoogleOptimize = () => {
+  const context = useContext(GoogleOptimizeContext);
+
+  if (!context) {
+    throw new Error("Component must be a child of WithGoogleOptimize to use useGoogleOptimize");
+  }
+
+  return context;
+};
